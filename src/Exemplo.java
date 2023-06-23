597d6de11319ae5079cc4ef93f6841d671088acb
Gabriel <gabrieldelconti99@gmail.com>
2023-06-23 14:29:59 -0300

    Adicionado helloManyTimes

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -1,9 +1,15 @@
 public class Exemplo {
+    private static void sayHelloManyTimes(int times) {
+        for (int i = 0; i < times; i++) {
+            sayHello();
+        }
+    }
+
     private static void sayHello() {
         System.out.println("Olá Mundo!");
     }
 
     public static void main(String[] args) {
-        sayHello();
+        sayHelloManyTimes(5);
     }
 }
